b85d0bc7208a0580219699f0e01584562957b3f0
Kirans-143 <kiran.shet77@gmail.com>
2024-10-28 20:46:45 +0530

    Basic web page for practice DOM

---
+++ Basic/app.js
@@ -0,0 +1,10 @@
+
+// getElementByClassName
+/* let smallImage = document.getElementsByClassName("oldImg");
+
+for (let i = 0; i < smallImage.length; i++) {
+  smallImage[i].src = "/assets/spiderman.jpeg";
+  console.log(`value of ${i} is changed`);
+}
+*/
+
